b91693e9cde8439c34484efa3f6f199552619b13
Dmitriy Skrylnikov <skrylnikovd@ya.ru>
2022-01-30 17:03:24 +0300

    fix img in post

--- server/config.ts
+++ server/config.ts
@@ -4,5 +4,6 @@ import { resolve } from 'path';
 export const rootDir: string = process.env.PWD || '';
 
 export const contentDir = resolve(rootDir, 'content');
+export const publicDir = resolve(rootDir, 'public');
 
 export const blogDir = resolve(rootDir, 'client', 'pages', 'post');
--- server/content.ts
+++ server/content.ts
@@ -1,12 +1,48 @@
 import * as fs from 'fs';
 import * as http from 'isomorphic-git/http/node';
-import { mkdir, opendir, readFile, rm, writeFile } from 'fs/promises';
+import { mkdir, opendir, readFile, rm, writeFile, copyFile } from 'fs/promises';
 import { resolve } from 'path';
+import { createHash } from 'crypto';
+import { execSync } from 'child_process';
 
 import Git from 'isomorphic-git';
 import Yaml from 'yaml';
+import { last } from 'remeda';
 
-import { contentDir, blogDir } from './config';
+import { contentDir, blogDir, publicDir } from './config';
+
+const imageReg = /\.(gif|jpg|jpeg|png|webp)$/i;
+
+const assetsDir = resolve(publicDir, 'assets');
+
+
+const loadImg = async (dirPath: string) => {
+  const dir = await opendir(dirPath);
+
+  const result: Array<[string, string]> = [];
+
+  for await (const dirent of dir) {
+    if (dirent.isDirectory()) {
+      continue
+    };
+    if (!imageReg.test(dirent.name)) {
+      continue;
+    }
+
+    const ext = last(dirent.name.split('.'));
+
+    const hash = createHash('sha256');
+
+    const hashName = hash.digest('hex').slice(0, 20);
+
+
+    await copyFile(resolve(dirPath, dirent.name), resolve(assetsDir, hashName + '.' + ext));
+
+    result.push([`(./${dirent.name})`, `(/assets/${hashName}.${ext})`]);
+  }
+
+  return result;
+};
 
 export const loadContent = async () => {
   const isContentDirExist = fs.existsSync(contentDir);
@@ -33,6 +69,11 @@ export const loadContent = async () => {
 
   await mkdir(blogDir);
 
+  if (fs.existsSync(assetsDir)) {
+    await rm(assetsDir, { recursive: true });
+  }
+  await mkdir(assetsDir);
+
   const contentBlogDir = resolve(contentDir, 'blog');
 
   const dir = await opendir(contentBlogDir);
@@ -48,13 +89,20 @@ export const loadContent = async () => {
 
       const meta = Yaml.parse(parsedFile[1]);
 
-      console.log(meta);
+      const parsedContent = parsedFile[2].split('\n');
 
+      parsedFile[2] = parsedContent.slice(parsedContent.findIndex((x) => x.includes('# ')) + 1).join('\n');
+
+      const assetList = await loadImg(resolve(contentBlogDir, dirent.name));
+
+      assetList.forEach(([source, target]) => {
+        parsedFile[2] = parsedFile[2].replace(source, target);
+      })
 
       await writeFile(resolve(blogDir, meta.url + '.md'), parsedFile.join('---'), { encoding: 'utf-8' })
     }
   }
 
 
-
+  execSync('pnpm build')
 };
