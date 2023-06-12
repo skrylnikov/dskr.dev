cfa1f3312dc15edce7e9360157be6b07528e1daa
Dmitriy Skrylnikov <skrylnikovd@ya.ru>
2023-06-12 15:45:41 +0300

    add post about photo lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dskr.dev/',
 	integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      (x) => rehypeExternalLinks({ ...x, target: '_blank'})
+    ],
+    extendDefaultPlugins: true,
+  }
 });
-
