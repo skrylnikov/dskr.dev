225a2422c7a5dc5d12b56af64fbc55d32beb2ebb
Dmitriy Skrylnikov <skrylnikovd@ya.ru>
2023-09-19 00:53:31 +0300

    test

--- src/pages/_turbo.xml.ts
+++ src/pages/_turbo.xml.ts
@@ -22,6 +22,10 @@ export async function get(context: any) {
       // Compute RSS link from post `slug`
       // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/blog/${post.slug}/`,
+      'turbo:content': parser.render(post.body).replaceAll(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''),
+      "turbo:source": `/blog/${post.slug}/`,
+      "turbo:extendedHtml": true
 		})}),
+    
 	});
 }
